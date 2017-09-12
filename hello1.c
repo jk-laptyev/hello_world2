599e0e631aecfe995ea2ae97ab18ad4ea846d9af
jk <jk.laptyev@gmail.com>
2017-09-12 17:42:54 +0300

    placed both modules into one folder.

---
+++ hello1.c
@@ -0,0 +1,25 @@
+#include <linux/module.h>	/* Needed by all modules */
+#include <linux/kernel.h>	/* Needed for KERN_INFO */
+//#include <linux/printk.h>
+MODULE_LICENSE("GPL");
+
+static int __init hello_init(void)
+{
+    printk(KERN_INFO "Hello world printer loaded.\n");
+    return 0;
+}
+
+static void __exit hello_exit(void)
+{
+    printk(KERN_INFO "Hello world printer quit.\n");
+}
+
+static int print_hello(void) {
+    printk(KERN_EMERG "Hello world!\n");
+    return 0;
+}
+
+EXPORT_SYMBOL(print_hello);
+
+module_init(hello_init);
+module_exit(hello_exit);
